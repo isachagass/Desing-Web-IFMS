5e9623aa8e2cbe05e0e94705a8678fbc4a51a1b1
Isabelli C. Kubo <163148600+isachagass@users.noreply.github.com>
2026-08-21 21:26:48 -0400

    Add files via upload

---
+++ semestre2/script1.js
@@ -0,0 +1,61 @@
+// condição ? valor verdadeiro : valor Falso
+// let numeros = [];
+
+// for (let i = 0; i <= 5; i++){
+//     numeros[i] = i*2
+// }
+
+
+// for (const num of numeros){
+//     console.log("numero: "+num);
+// }
+
+// for (const letra of "isabelli"){
+//     console.log(letra)
+// }
+
+// for (let i=0; i<=10; i++){
+//     for (let j=0;j<=10; j++){
+//         console.log(i +"*"+j+" = "+i*j)
+//     }
+    // console.log('\n')
+// }
+
+// console.log("Contagem regressiva....")
+// for (let i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+// treinar do while
+// quando uma function n devove nadda ela é chamada de procedimento
+// function anonima: não tem nome, vai direto de uma variavel
+
+// arrow function: quando ela for usada uma vez só
+// const sub = (a,b) => a-b;
+
+// callback
+
+
+// function e_par(num){
+//     if (num % 2 == 0){
+//         return "é par"
+//     }
+//     else{
+//         return "é impar"
+//     }
+// }
+// par = e_par(20)
+// console.log(par)
+
+// const numero_par = function (a) {
+//    return a % 2;
+// }
+// let par = numero_par(9);
+// par == 0 ? console.log("é par") : console.log("é impar");
+
+// console.log(par)
+
+
+// const par_semnome = (num) => num%2;
+// par = par_semnome(10)
+// par == 0 ? console.log("é par") : console.log("é impar");
